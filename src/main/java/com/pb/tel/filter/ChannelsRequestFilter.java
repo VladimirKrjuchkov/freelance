828152d510a5949f5a799ae4ca3287ccd50292f9
vladimir <vladimir.krjuchkov.privatbank.ua>
2018-07-09 14:52:46 +0300

    stream instead of cycle

---
+++ src/main/java/com/pb/tel/filter/ChannelsRequestFilter.java
@@ -0,0 +1,34 @@
+package com.pb.tel.filter;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pb.tel.data.channels.ChannelsRequest;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by vladimir on 09.07.18.
+ */
+public class ChannelsRequestFilter implements Filter {
+
+    private final Logger log = Logger.getLogger(ChannelsRequestFilter.class.getCanonicalName());
+
+    private static ObjectMapper jacksonObjectMapper = new ObjectMapper();
+
+    public void init(FilterConfig arg0) throws ServletException {}
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        ChannelsRequest channelsRequest = jacksonObjectMapper.readValue(request.getReader(), ChannelsRequest.class);
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+        if("operatorStatus".equalsIgnoreCase(channelsRequest.getAction())){
+            httpResponse.sendError(405);
+        }else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    public void destroy() {}
+}
